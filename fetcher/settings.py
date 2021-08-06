f819800b0c0827a7d3c6fdaf8085b062681c095d
thisissandy <sandysingh9449@gmail.com>
2021-08-06 23:53:58 +0530

    solve exposed secret key

--- fetcher/settings.py
+++ fetcher/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x33!gty!og6be_!g(ho+_rrrq0mt0+3h0h3784c^34r-p_5lfa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
